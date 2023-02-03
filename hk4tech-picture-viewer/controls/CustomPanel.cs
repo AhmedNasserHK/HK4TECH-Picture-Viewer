69540b5cc2e4f51af3d194494d501e0228696f60
Ahmed Nasser HK <ahmed.play94@gmail.com>
2023-02-03 17:29:50 +0200

    initial release
    initial release

---
+++ hk4tech-picture-viewer/controls/CustomPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows.Forms;
+
+namespace hk4tech_picture_viewer
+{
+	class CustomPanel : Panel
+	{
+		public CustomPanel() { }
+
+		protected override void OnMouseWheel(MouseEventArgs e)
+		{
+			if (VScroll && (ModifierKeys & Keys.Shift) == Keys.Shift && hk4tech_picture_viewer.Properties.Settings.Default.MouseWheelScrollAction == 0)
+			{
+				VScroll = false;
+				base.OnMouseWheel(e);
+				VScroll = true;
+			}
+			else if (
+				(VScroll && (ModifierKeys & Keys.Control) == Keys.Control) || 
+				hk4tech_picture_viewer.Properties.Settings.Default.MouseWheelScrollAction == 1 ||
+				hk4tech_picture_viewer.Properties.Settings.Default.MouseWheelScrollAction == 2
+			)
+			{
+				VScroll = false;
+				HScroll = false;
+				base.OnMouseWheel(e);
+				VScroll = true;
+				HScroll = true;
+			}
+			else
+			{
+				base.OnMouseWheel(e);
+			}
+		}
+	}
+}
